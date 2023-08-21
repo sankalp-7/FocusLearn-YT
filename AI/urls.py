d31d582b4883d28199113e70f38c82da31314e0c
Sankalp <104098061+sankalp-7@users.noreply.github.com>
2023-08-21 08:33:26 +0530

    added quiz genration and summarzation logic

--- AI/urls.py
+++ AI/urls.py
@@ -1,8 +1,11 @@
-from django.urls import path,include
+from django.urls import path,include,re_path
 from.views import *
 app_name='AI'
 
 urlpatterns=[
     path('',home,name='home'),
     path('get-videos/',get_videos),
+    path('summarize-view/',summarize_view),
+    path('quiz-view/',quiz_view),
+
 ]
--- AI/views.py
+++ AI/views.py
@@ -2,10 +2,18 @@ from django.shortcuts import render
 from apiclient.discovery import build
 from django.http import JsonResponse
 import json
+import re
+from youtube_transcript_api import YouTubeTranscriptApi
+import torch
+import torchaudio
+import openai
+import textwrap
+from transformers import pipeline
 # Create your views here.
 
 api_key="AIzaSyCRzrt-0rHNQ4DzybpAeWSO_q7SyDR2OJo"
 youtube=build('youtube','v3',developerKey=api_key)
+openai.api_key = "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"
 def home(request):
     return render(request,'AI/home.html')
 
@@ -14,5 +22,75 @@ def get_videos(request):
         yt_query=request.POST['query']
         req=youtube.search().list(q=yt_query,part='snippet',type='video')
         res=req.execute()
-        return render(request, 'AI/main.html', {'video_data': res})
+        return render(request, 'AI/main.html', {'video_data': res,'q':yt_query})
     return render(request,'AI/main.html')
+
+def summarize_view(request):
+    video_url = request.GET.get('url')
+    if video_url:
+        #summarization logic using the video_url
+        youtube_url="https://www.youtube.com/watch?v="+video_url
+        match = re.search(r"v=([A-Za-z0-9_-]+)", youtube_url)
+        if match:
+            video_id = match.group(1)
+        else:
+            raise ValueError("Invalid YouTube URL")
+
+        transcript = YouTubeTranscriptApi.get_transcript(video_id)
+
+        transcript_text = ""
+        for segment in transcript:
+            transcript_text += segment["text"] + " "
+        def split_text_into_chunks(text, max_chunk_size):
+            return textwrap.wrap(text, max_chunk_size)
+        max_chunk_size = 4000
+
+        transcript_chunks = split_text_into_chunks(transcript_text, max_chunk_size)
+        summaries = ""
+
+        for chunk in transcript_chunks:
+            response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo-16k",
+            messages=[
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": f"{chunk}\n\nCreate short concise summary"}
+            ],
+            max_tokens=250,
+            temperature=0.5
+        )
+
+        summaries += response['choices'][0]['message']['content'].strip() + " "
+        print(summaries)
+
+        # Render your template with the summary
+        return render(request, 'AI/summary.html', {'summary': summaries})
+
+    return JsonResponse({'error': 'Invalid request.'})
+
+
+def quiz_view(request):
+    video_url = request.GET.get('url')
+    if video_url:
+        youtube_url="https://www.youtube.com/watch?v="+video_url
+        match = re.search(r"v=([A-Za-z0-9_-]+)", youtube_url)
+        if match:
+            video_id = match.group(1)
+        else:
+            raise ValueError("Invalid YouTube URL")
+
+        transcript = YouTubeTranscriptApi.get_transcript(video_id)
+        transcript_text = ""
+        response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo-16k",
+        messages=[
+        {"role": "system", "content": "You are a helpful assistant that generates questions."},
+        {"role": "user", "content": transcript_text},
+        {"role": "user", "content": "Generate 10 quiz questions based on the text with multiple choices.Avoid questions which are too easy or are irrelevant.Avoid questions which require a picture to solve them.create them in json format"},
+        ]
+        )
+        quiz_questions = response['choices'][0]['message']['content']
+        print("Quiz Questions:")
+        print(quiz_questions)
+        return render(request,'AI/quiz.html',{'quiz':quiz_questions})
+    return JsonResponse({'error': 'Invalid request.'})
+
